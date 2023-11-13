cd32b7230964e33974302706f9874157a591f5c0
woomae <cwg0809@naver.com>
2023-11-13 17:37:42 +0900

    flask set
    flask set

---
+++ chatbot/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+from assistant import callchat
+load_dotenv()
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    if request.method == 'POST':
+        messages = request.get_json()
+        if 'message' in messages:
+            user_message = messages['message']
+            result = callchat(user_message)
+            return result
+    return 'Invalid request'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000) #서비스시 host값 ip로 변경, port도 변경, route경로도 확인할것
--- chatbot/assistant.py
+++ chatbot/assistant.py
@@ -1,40 +1,38 @@
 import os
 import time
 from openai import OpenAI
-
 from dotenv import load_dotenv
 
-load_dotenv()
 
-client = OpenAI(
-    api_key=os.environ['OPENAI_API_KEY'],
-)
-
-#스레드 생성,연결(runs)
-run = client.beta.threads.create_and_run(
-    assistant_id="asst_FPg6Rlt9WKrgjROV07Du4joO", #본인 assistant id 입력
-    thread={
-    "messages": [
-        {"role": "user", "content": "어떤 검색어가 가장 많이 검색됐어?"}
-    ]
-    }
-)
-#3초 대기
-time.sleep(3)
-#실행 단계 나열
-run_steps = client.beta.threads.runs.steps.list(
-    thread_id=run.thread_id,
-    run_id=run.id
-)
-#실행 단계 검색
-run_step = client.beta.threads.runs.steps.retrieve(
-    thread_id=run.thread_id,
-    run_id=run.id,
-    step_id=run_steps.data[0].id
-)
-#메세지 검색
-message = client.beta.threads.messages.retrieve(
-    message_id=run_step.step_details.message_creation.message_id,
-    thread_id=run.thread_id,
-)
-print(message.content[0].text.value)
+def callchat(message):
+    client = OpenAI(
+        api_key=os.environ['OPENAI_API_KEY'],
+    )
+    #스레드 생성,연결(runs)
+    run = client.beta.threads.create_and_run(
+        assistant_id="asst_FPg6Rlt9WKrgjROV07Du4joO", #본인 assistant id 입력
+        thread={
+        "messages": [
+            {"role": "user", "content": message},
+        ]
+        }
+    )
+    #3초 대기
+    time.sleep(3)
+    #실행 단계 나열
+    run_steps = client.beta.threads.runs.steps.list(
+        thread_id=run.thread_id,
+        run_id=run.id
+    )
+    #실행 단계 검색
+    run_step = client.beta.threads.runs.steps.retrieve(
+        thread_id=run.thread_id,
+        run_id=run.id,
+        step_id=run_steps.data[0].id
+    )
+    #메세지 검색
+    message = client.beta.threads.messages.retrieve(
+        message_id=run_step.step_details.message_creation.message_id,
+        thread_id=run.thread_id,
+    )
+    return message.content[0].text.value
