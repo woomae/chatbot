4894c651c622947385e5936fb3abd4d64d854763
woomae <cwg0809@naver.com>
2023-11-13 13:23:21 +0900

    cleardata
    cleardata

--- chatbot_test/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import openai
-
-from dotenv import load_dotenv
-
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-fine_tune_events = openai.FineTune.list_events(id='file-odC56jS9MUtCqcmPp9YWjWRM')
